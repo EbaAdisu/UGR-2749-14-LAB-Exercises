e9807a68bb602c03244779efd3cce1d24c83f624
Eba Adisu <ebaadisu2@gmail.com>
2024-03-08 15:29:02 +0300

    LAB-1- Classes-and-Objects-One-Exercise-1

--- LAB-1- Classes-and-Objects-One-Exercise-1.dart
+++ LAB-1- Classes-and-Objects-One-Exercise-1.dart
@@ -1,8 +1,8 @@
 void main() {
-  var person = Person();
-  person.name = 'John';
+  var person = Person('Ebba', 25, 'Ethiopia');
+  person.name = 'Bereket';
   person.age = 30;
-  person.address = 'New York';
+  person.address = 'USA';
   print('Name: ${person.name}');
   print('Age: ${person.age}');
   print('Address: ${person.address}');
@@ -12,4 +12,5 @@ class Person {
   String? name;
   int? age;
   String? address;
+  Person(this.name, this.age, this.address);
 }

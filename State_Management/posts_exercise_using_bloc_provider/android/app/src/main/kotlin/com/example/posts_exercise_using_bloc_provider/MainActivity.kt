11b51247198b4cfe9812386e114b320b1b971967
Eba Adisu <ebaadisu2@gmail.com>
2024-04-21 13:17:42 +0300

    Post exercise

---
+++ State_Management/posts_exercise_using_bloc_provider/android/app/src/main/kotlin/com/example/posts_exercise_using_bloc_provider/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.posts_exercise_using_bloc_provider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ State_Management/posts_exercise_using_rivepod_provider/android/app/src/main/kotlin/com/example/posts_exercise_using_rivepod_provider/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.posts_exercise_using_rivepod_provider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
